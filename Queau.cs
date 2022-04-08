1154ab3690f66b89e2a9e9e973e65a80142c9941
sriranjani19 <99411199+sriranjani19@users.noreply.github.com>
2022-04-08 10:36:02 +0530

    program

---
+++ Queau.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stackqu
+{
+    public class Queau
+    {
+        public static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+
+
+
+            foreach (int i in queue)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
+
+    
+
---
+++ peek.cs
@@ -0,0 +1,58 @@
+﻿
+
+
+using System;
+namespace UC_2_PeakPop
+
+{
+    class Program
+    {
+        /// <summary>
+        /// UC2 Stack 70,30,56 values are Peek, Pop and empty the stack.
+        /// </summary>
+
+        static void Main(string[] args)
+        {
+            Stack<int> st = new Stack<int>();
+            st.Push(70);
+            st.Push(30);
+            st.Push(56);
+
+            Console.WriteLine("Before Pop the elements is");
+
+            foreach (int i in st)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("the pick element is=" + st.Peek());
+
+            Console.WriteLine("After 1st time pop elements is");
+
+            st.Pop();
+
+            foreach (int i in st)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("the pick element is=" + st.Peek());
+
+            Console.WriteLine("After 2st time pop elements");
+            st.Pop();
+
+            foreach (int i in st)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("the pick element is=" + st.Peek());
+
+            Console.WriteLine("After 3st time pop elements=0");
+            st.Pop();
+
+            foreach (int i in st)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Now stack is empty and Peek values also 0");
+        }
+    }
+}
